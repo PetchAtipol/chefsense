9c706282b0822a58ef325a3b61a9f8796803e092
Eclair2199 <53sdpetch@gmail.com>
2025-02-17 23:49:19 +0700

    first commit

--- pages/firebase.jsx
+++ pages/firebase.jsx
@@ -6,12 +6,12 @@ import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAV4bbFky3zA5txgplrJxz8X0zqnz2K7aI",
-  authDomain: "uploadingfile-petch.firebaseapp.com",
-  projectId: "uploadingfile-petch",
-  storageBucket: "uploadingfile-petch.firebasestorage.app",
-  messagingSenderId: "587733213944",
-  appId: "1:587733213944:web:e89728122e79eec911d10d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
